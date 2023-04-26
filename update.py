f88b6d4535d6eba9f5d3f13479d7ccd6a6d4a211
ChrisHairstonBnR <103683006+ChrisHairstonBnR@users.noreply.github.com>
2023-04-26 17:35:21 -0400

    Changed API Access Code

--- update.py
+++ update.py
@@ -1,11 +1,15 @@
-from github import *
-
+import requests
 class Updater:
     def __init__(self):
-        repo = Github('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', verify=False).get_repo('ChrisHairstonBnR/Python-Successor-Finder')
-        releases = repo.get_releases()
-        self.latestVersion = releases[0].title
-        #self.latestVersion = repo.get_latest_release().title
-        self.latestVersionLink = releases[0].zipball_url
+        # repo = Github(login_or_token='Iv1.5aae7b1280a26a63', password='77b68bc95eca67303216027b84d73be8db46d4b9', verify=False).get_repo('ChrisHairstonBnR/Python-Successor-Finder')
+        # releases = repo.get_releases()
+        # self.latestVersion = releases[0].title
+        # self.latestVersionLink = releases[0].zipball_url
+        
+        response = requests.get("https://api.github.com/repos/ChrisHairstonBnR/Python-Successor-Finder/releases", verify=False)
+        responseJSON = response.json()
+        self.latestVersion = responseJSON[0]['tag_name']
+        self.latestVersionLink = responseJSON[0]['assets'][0]['browser_download_url']
+
         
         
